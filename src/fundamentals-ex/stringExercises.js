fecf320f6295e8078f8a69af8324eff73a485a4d
prlongoria <prlongoria@gmail.com>
2022-06-10 13:48:47 +0200

    init repo

---
+++ src/fundamentals-ex/stringExercises.js
@@ -0,0 +1,50 @@
+export function returnHello()
+{
+    return"Hello";
+}
+
+export function returnAString()
+{
+    /* Completa la function */
+    
+    
+}
+
+export function concatenateStrings()
+{
+    /* Completa la function */
+    const firstWord = 'I';
+    const secondWord = 'Love';
+    
+}
+
+export function giveOneLetter()
+{
+    /* Completa la function */
+    const dog = 'dog';
+}
+
+export function giveTheIndexOfWord()
+{
+    const wordToSearch = 'Butterfly';
+    const phrase = "Once upon a time!!!";
+    
+}
+
+export function replaceFishWords()
+{
+    let phrase = 'Give a Man a Fish';
+    
+}
+
+export function giveAllLettersInUppercase()
+{
+    let phrase = 'Ride Him, Cowboy!';
+    
+}
+
+export function removeWhiteSpaces()
+{
+    let phrase = '    Hands Down    ';
+    
+}
